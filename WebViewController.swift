c16821a54bcbad5dd57b9377136549aa4627a8a5
JunxiBao <baojunxilogin@icloud.com>
2025-08-08 13:17:53 +0800

    增加失败报错

--- WebViewController.swift
+++ WebViewController.swift
@@ -12,10 +12,31 @@ class WebViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        // 设置导航代理以处理加载失败
+        webView.navigationDelegate = self
+
+        // 加载线上地址
         if let url = URL(string: "https://zhucan.xyz") {
             let request = URLRequest(url: url)
             webView.load(request)
         }
     }
-} 
+}
+
+// MARK: - WKNavigationDelegate
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        showErrorAlert(error: error)
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        showErrorAlert(error: error)
+    }
+    
+    private func showErrorAlert(error: Error) {
+        let alert = UIAlertController(title: "加载失败", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "确定", style: .default))
+        present(alert, animated: true)
+    }
+}
